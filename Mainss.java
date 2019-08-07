9d96d523ae024cf8686cc31dd1f688dce4189a4d
sususama <1023605039@qq.com>
2019-08-07 12:43:29 +0800

    8/7

---
+++ Mainss.java
@@ -0,0 +1,18 @@
+package PTA;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int i=0;i<n;i++) {
+			BigInteger a=in.nextBigInteger();
+			BigInteger b=in.nextBigInteger();
+			BigInteger c=in.nextBigInteger();
+			//BigInteger d=new BigInteger (a.add(b));
+		}
+}
+}
