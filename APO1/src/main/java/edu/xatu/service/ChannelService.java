89e440135ed4c29459883a1eb82e067bdf6305c0
王旭昊 <1023605039@qq.com>
2019-10-31 09:00:46 +0800

    10/31

---
+++ APO1/src/main/java/edu/xatu/service/ChannelService.java
@@ -0,0 +1,11 @@
+package edu.xatu.service;
+
+import edu.xatu.entity.Channel;
+
+public interface ChannelService {
+    public void addChannel(Channel channel);
+    /**
+     * 转帐
+     */
+    public void zhuangZhang();
+}
