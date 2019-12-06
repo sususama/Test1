1cf716a382da461f164d97383ab9d5281786eba8
王旭昊 <1023605039@qq.com>
2019-12-06 09:37:30 +0800

    12/6

---
+++ CList/cmake-build-debug/CMakeFiles/CList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CList.exe"
+  "CList.exe.manifest"
+  "CList.pdb"
+  "CMakeFiles/CList.dir/main.c.obj"
+  "libCList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
