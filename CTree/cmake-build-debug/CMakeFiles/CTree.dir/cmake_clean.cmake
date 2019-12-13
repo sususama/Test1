6feb16ccc627e3574f31ad934eab905e03bc7371
王旭昊 <1023605039@qq.com>
2019-12-13 18:58:06 +0800

    12/13

---
+++ CTree/cmake-build-debug/CMakeFiles/CTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CTree.dir/main.c.obj"
+  "CTree.exe"
+  "CTree.exe.manifest"
+  "CTree.pdb"
+  "libCTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
