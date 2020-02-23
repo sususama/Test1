744f49b745e48c54a405f9e3b0e31d3cb88cd1b5
王旭昊 <1023605039@qq.com>
2020-02-23 14:52:51 +0800

    2/23

---
+++ buke/Test1.java
@@ -0,0 +1,13 @@
+package buke;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		Person p= new China();
+		Cock c=new Cock();
+		p.eat(c);
+		Duck d=new Duck();
+		p.eat(d);
+	}
+
+}
