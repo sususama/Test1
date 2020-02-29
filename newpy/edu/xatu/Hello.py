0cd0056e086cee83ef950d5a76a751706b48d588
王旭昊 <1023605039@qq.com>
2020-02-29 17:06:42 +0800

    2/29

---
+++ newpy/edu/xatu/Hello.py
@@ -0,0 +1,7 @@
+full_name="John Smith";
+age=20;
+is_new=True;
+name1=input("what is your name?\n");
+print("H1 "+name1);
+color1=input("what\'s your favorite color\n");
+print(name1+" favorite like color is "+color1);
