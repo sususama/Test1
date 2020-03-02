cb8c510359023074289dd191167effd17e63fb32
王旭昊 <1023605039@qq.com>
2020-03-02 18:52:01 +0800

    3/2

---
+++ newpy/edu/xatu/ex.py
@@ -0,0 +1,3 @@
+x = 10
+if x > 5:
+    raise Exception('x 不能大于 5。x 的值为: {}'.format(x))
---
+++ newpy/edu/xatu/wordgame.py
@@ -0,0 +1,7 @@
+temp=input("猜猜我现在想的是那个数字:\n");
+guess=int(temp);
+if(guess==8):
+    print("猜对了");
+else:
+    print("猜错了,数字是8");
+print("游戏结束");
