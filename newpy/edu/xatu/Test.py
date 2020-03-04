88ef94275ac972ed8b233d03381246d8701a9a19
王旭昊 <1023605039@qq.com>
2020-03-04 14:25:15 +0800

    3/4

---
+++ newpy/edu/xatu/Test.py
@@ -0,0 +1,19 @@
+l = [('清华退学王垠 VS 阿里P10 赵海平，这场面试之战到底谁赢了？', 'IT头条', 'https://www.lmonkey.com/t/OxLPRxAEe'),
+     ('美国宣布限制 AI 软件出口中国：明日生效，无人机、自动驾驶首当其冲', 'IT头条', 'https://www.lmonkey.com/t/87y4krQE3'),
+     ('腾讯7年老员工遭暴力裁员，官方回应：该员工工作成果不匹配岗位要求', 'IT头条', 'https://www.lmonkey.com/t/59ElQAqyR'),
+     ('架构图解', 'IT头条', 'https://www.lmonkey.com/t/onBzqqML2'),
+     ('微服务架构的四大杀手锏', 'IT头条', 'https://www.lmonkey.com/t/oREQAgkE1'),
+     ('会员服务在高可用架构的实战探索', 'IT头条', 'https://www.lmonkey.com/t/RwykQ74BX'),
+     ('十年老网工突然提离职，我该如何劝阻？', 'IT头条', 'https://www.lmonkey.com/t/2zLAPo8yW'),
+     ('从限高令到冻结令，熊猫之父王思聪用数十亿卖来的教训', 'IT头条', 'https://www.lmonkey.com/t/2QL7jY9yk'),
+     ('阿里巴巴为什么能抗住90秒100亿？看完这篇你就明白了！', 'IT头条', 'https://www.lmonkey.com/t/GPE69ZxBg'),
+     ('盗版12306骗3000万人下载，暴利高仿App是如何花式捞钱的？', 'IT头条', 'https://www.lmonkey.com/t/2QL7jOxyk'),
+     ('阿里毕玄：从生物系学生，到技术团队 leader，他是如何完成自我蜕变的', 'IT头条', 'https://www.lmonkey.com/t/7VEZm2kB3'),
+     ('分布式事务实战', 'IT头条', 'https://www.lmonkey.com/t/DlL3pgxBn'),
+     ('我在职场第一次薪资翻倍的经历！', 'IT头条', 'https://www.lmonkey.com/t/naypeqKLK'),
+     ('美团下一代服务治理系统 OCTO2.0 的探索与实践', 'IT头条', 'https://www.lmonkey.com/t/MXyGoWqyp')]
+l1 = [(1, 'akjsdh'), (2, 'akjsdh'), (3, 'akjsdh')]
+s = ','.join(str(i) for i in l1)
+print(s)
+print(type(s))
+print(type(l1))
---
+++ newpy/edu/xatu/app.py
@@ -0,0 +1,23 @@
+# birth_year=input("Birth year:\n")
+# birth_year=int(birth_year)
+# age=2019-birth_year
+# print(age)
+# course="Python for Beginners"
+# print(course[:5])
+# name="Jennifer"
+# print(name[1:-1])
+# def changme(myList):
+#     myList.append([1,2,3,4])
+#     print("函数内取值",myList)
+#     return
+# myList=[10,20,30]
+# changme(myList)
+# print("外取值",myList)
+# global myList
+# import math
+# content=dir(math)
+# print(content)
+# first='John'
+# last='Smith'
+# msg=f"{first} [{last}] is a coder"
+# print(msg)
